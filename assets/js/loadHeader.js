ce2058397c39c08a2ac0e85d338fc85aa242ccc6
Jean Giraldo <jeann.giraldo@gmail.com>
2026-06-16 11:25:04 -0500

    add light-mode toggle

--- assets/js/loadHeader.js
+++ assets/js/loadHeader.js
@@ -4,4 +4,23 @@ fetch(headerPath)
 	.then(response => response.text())
 	.then(html => {
 	document.body.insertAdjacentHTML("afterbegin", html);
+
+	const current_theme = localStorage.getItem("jeangiraldooTheme")
+	if (!current_theme) {
+		localStorage.setItem("jeangiraldooTheme", "dark-mode")
+	} else {
+		document.body.classList.add(current_theme);
+	}
+
+	const btn = document.getElementById("theme-toggle")
+
+	btn.addEventListener("click", () => {
+		document.body.classList.toggle("light-mode");
+		if (document.body.classList.contains("light-mode")) {
+			localStorage.setItem("jeangiraldooTheme", "light-mode")
+		} else {
+			localStorage.setItem("jeangiraldooTheme", "dark-mode")
+		}
+	});
+
 });
